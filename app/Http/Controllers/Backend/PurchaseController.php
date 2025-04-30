2492c8605654e03a1d2713504cbed3276d30695e
easylearningbd <elebdsoft@gmail.com>
2025-05-01 02:29:53 +0600

    c51 Manage Purchases System Part 2

--- app/Http/Controllers/Backend/PurchaseController.php
+++ app/Http/Controllers/Backend/PurchaseController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use App\Models\Product; 
+use App\Models\Supplier; 
+use App\Models\WareHouse;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use App\Models\Purchase; 
+use App\Models\PurchaseItem; 
 
 class PurchaseController extends Controller
 {
-    //
+    public function AllPurchase(){
+        $allData = Purchase::orderBy('id','desc')->get();
+        return view('admin.backend.purchase.all_purchase',compact('allData')); 
+    }
+    // End Method 
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\WareHouseController;
 use App\Http\Controllers\Backend\SupplierController;
 use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\PurchaseController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -93,6 +94,11 @@ Route::controller(ProductController::class)->group(function(){
     Route::post('/update/product', 'UpdateProduct')->name('update.product');
     Route::get('/delete/product/{id}', 'DeleteProduct')->name('delete.product');
     Route::get('/details/product/{id}', 'DetailsProduct')->name('details.product');
+    
+});
+
+Route::controller(PurchaseController::class)->group(function(){
+    Route::get('/all/purchase', 'AllPurchase')->name('all.purchase'); 
     
 });
 
