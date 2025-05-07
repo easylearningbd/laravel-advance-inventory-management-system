1be409d124c75e9842a4d549c146613c8b19feaf
easylearningbd <elebdsoft@gmail.com>
2025-05-08 04:22:38 +0600

    c107 Transfer Product Details Page

--- routes/web.php
+++ routes/web.php
@@ -172,6 +172,7 @@ Route::controller(TransferController::class)->group(function(){
     Route::get('/edit/transfer/{id}', 'EditTransfer')->name('edit.transfer');
     Route::post('/update/transfer/{id}', 'UpdateTransfer')->name('update.transfer');
     Route::get('/delete/transfer/{id}', 'DeleteTransfer')->name('delete.transfer');
+    Route::get('/details/transfer/{id}', 'DetailsTransfer')->name('details.transfer');
       
      
 });
