3da27419505ad6d223a84f793000240bb1e1c6a1
easylearningbd <elebdsoft@gmail.com>
2025-05-09 03:55:30 +0600

    c113 Report Management Part 6

--- app/Http/Controllers/Backend/ReportController.php
+++ app/Http/Controllers/Backend/ReportController.php
@@ -12,6 +12,7 @@ use App\Models\SaleReturn;
 use Illuminate\Support\Facades\DB; 
 use App\Models\Purchase; 
 use App\Models\ReturnPurchase; 
+use Carbon\Carbon;
 
 class ReportController extends Controller
 {
@@ -20,6 +21,24 @@ class ReportController extends Controller
         return view('admin.backend.report.all_report',compact('purchases')); 
     }
     // End Method 
+
+    public function FilterPurchases(Request $request){
+
+        $startDate = $request->input('start_date');
+        $endDate = $request->input('end_date');
+        $query = Purchase::with(['purchaseItems.product','supplier','warehouse']);
+
+        if ($startDate && $endDate ) {
+            $startDate = Carbon::parse($startDate)->startOfDay();
+            $endDate = Carbon::parse($endDate)->endOfDay();
+            $query->whereBetween('date',[$startDate,$endDate]);
+        }
+
+        $purchases = $query->get();
+        return response()->json(['purchases' => $purchases]);
+
+    }
+     // End Method 
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -180,7 +180,9 @@ Route::controller(TransferController::class)->group(function(){
 
 
 Route::controller(ReportController::class)->group(function(){
-    Route::get('/all/report', 'AllReport')->name('all.report');   
+    Route::get('/all/report', 'AllReport')->name('all.report'); 
+    
+    Route::get('/filter-purchases', 'FilterPurchases')->name('filter-purchases'); 
 
 });
 
