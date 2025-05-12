0b2f40f2ba2286997f4e278dd6055e0523ad6c7c
easylearningbd <elebdsoft@gmail.com>
2025-05-13 02:23:38 +0600

    c129 Add Roles in Permission Part 5

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -242,6 +242,9 @@
     <li>
         <a href="{{ route('add.roles.permission') }}" class="tp-link">Role In Permission</a>
     </li>
+    <li>
+        <a href="{{ route('all.roles.permission') }}" class="tp-link">All Role Permission</a>
+    </li>
     
 </ul>
         </div>
